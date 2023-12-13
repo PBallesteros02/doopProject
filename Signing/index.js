8f801f4b3a4fc7da4498eb58c86ee60ad7d859c6
Priscilla Ballesteros <89608053+PBallesteros02@users.noreply.github.com>
2023-12-12 21:43:37 -0800

    Create index.js

---
+++ Signing/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="container">
+      <div className="inner-container">
+        <div className="welcome-text">Welcome</div>
+        <button className="button button-logout">Logout</button>
+        <button className="button button-create-post">Create Post</button>
+        <button className="button button-feed">Feed</button>
+      </div>
+    </div>
