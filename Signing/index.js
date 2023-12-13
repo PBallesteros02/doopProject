6726c02ea9a83a9c1afabe9708c485b6f402650d
Priscilla Ballesteros <89608053+PBallesteros02@users.noreply.github.com>
2023-12-12 23:50:48 -0800

    Update index.js

--- Signing/index.js
+++ Signing/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./style.css";
 
-export const Index = ({ username }) => {
+export const Index = ({ id }) => {
   return (
     <div className="container">
       <div className="inner-container">
-        <div className="welcome-text">Welcome {username}! </div>
+        <div className="welcome-text">Welcome {id}! </div>
         <button className="button button-logout">Logout</button>
         <button className="button button-create-post">Create Post</button>
         <button className="button button-feed">Feed</button>
