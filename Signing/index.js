0d8aef260ee4b0cde6f9238a0d5eadf081d73cae
Priscilla Ballesteros <89608053+PBallesteros02@users.noreply.github.com>
2023-12-12 23:38:14 -0800

    Update index.js

--- Signing/index.js
+++ Signing/index.js
@@ -5,7 +5,7 @@ export const Index = () => {
   return (
     <div className="container">
       <div className="inner-container">
-        <div className="welcome-text">Welcome</div>
+        <div className="welcome-text">Welcome {username}! </div>
         <button className="button button-logout">Logout</button>
         <button className="button button-create-post">Create Post</button>
         <button className="button button-feed">Feed</button>
